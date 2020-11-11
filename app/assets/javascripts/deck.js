a0339247439382f93aaf83d0ccbcd0b0952392a0
Trevor Mast <trevormast@gmail.com>
2020-11-10 22:38:43 -0800

    adds weighted sample algorithm

--- app/assets/javascripts/deck.js
+++ app/assets/javascripts/deck.js
@@ -4,6 +4,14 @@ class Deck {
   }
 
   chooseCard() {
-    return this.cardsArray[Math.floor(Math.random() * this.cardsArray.length)];
+    // get a random card
+    let card = this.cardsArray[Math.floor(Math.random() * this.cardsArray.length)];
+    // get a random number between 0 and 100
+    let randomNumber = (Math.random() * 100);
+    // if the random number is between 0 and the card weight, return the card
+    if (randomNumber <= card.weight) { return card; }
+
+    // otherwise, try again
+    return this.chooseCard();
   }
 }
